f97a78a9063fde6e3545ac88f3c8a4f7469162de
WMXPY <wm@mengw.io>
2020-08-11 15:52:52 -0500

    rename type

--- src/cli.ts
+++ src/cli.ts
@@ -12,40 +12,40 @@ import { Version } from "./version";
 const coco = Coco.create();
 coco.globalOption(Option.create('spaces').setName('spaces'));
 
-type CommandType = {
+type CommandOption = {
 
     readonly path: string;
     readonly spaces?: string;
 };
 
-coco.command(Command.create('get').argument(Argument.create('path')).then(async (args: CommandType) => {
+coco.command(Command.create('get').argument(Argument.create('path')).then(async (args: CommandOption) => {
     const path: string = Path.resolve(args.path);
     const version: Version = await readConfig(path);
     console.log(version.toString());
 }));
 
-coco.command(Command.create('major').argument(Argument.create('path')).then(async (args: CommandType) => {
+coco.command(Command.create('major').argument(Argument.create('path')).then(async (args: CommandOption) => {
     const path: string = Path.resolve(args.path);
     const version: Version = await readConfig(path);
     const newVersion: Version = version.major();
     await writeConfig(path, newVersion, Number(args.spaces));
 }));
 
-coco.command(Command.create('minor').argument(Argument.create('path')).then(async (args: CommandType) => {
+coco.command(Command.create('minor').argument(Argument.create('path')).then(async (args: CommandOption) => {
     const path: string = Path.resolve(args.path);
     const version: Version = await readConfig(path);
     const newVersion: Version = version.minor();
     await writeConfig(path, newVersion, Number(args.spaces));
 }));
 
-coco.command(Command.create('patch').argument(Argument.create('path')).then(async (args: CommandType) => {
+coco.command(Command.create('patch').argument(Argument.create('path')).then(async (args: CommandOption) => {
     const path: string = Path.resolve(args.path);
     const version: Version = await readConfig(path);
     const newVersion: Version = version.patch();
     await writeConfig(path, newVersion, Number(args.spaces));
 }));
 
-coco.command(Command.create('auto').argument(Argument.create('path')).then(async (args: CommandType) => {
+coco.command(Command.create('auto').argument(Argument.create('path')).then(async (args: CommandOption) => {
     const path: string = Path.resolve(args.path);
     const version: Version = await readConfig(path);
     const newVersion: Version = version.auto();
